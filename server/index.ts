89a4a8589e161cab9042d10bd44aa82533933567
Ashentharinda <ashentharinda1998@gmail,com>
2025-09-18 14:04:06 +0530

    first commit

--- server/index.ts
+++ server/index.ts
@@ -61,11 +61,9 @@ app.use((req, res, next) => {
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
   const port = parseInt(process.env.PORT || '5000', 10);
-  server.listen({
-    port,
-    host: "0.0.0.0",
-    reusePort: true,
-  }, () => {
-    log(`serving on port ${port}`);
-  });
+const PORT = 5000; // or any free port
+server.listen(PORT, '0.0.0.0',() => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
+
 })();
